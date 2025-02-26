f64df45ab38716349435a28999bfbf0664496b86
ungsneelima <164307239+ungsneelima@users.noreply.github.com>
2025-02-26 11:42:02 +0530

    Create User

---
+++ User.java
@@ -0,0 +1,67 @@
+package com.sat.tmf.pmt.entity;
+
+import java.util.List;
+
+public class User {
+	private int user_id;
+	private String user_name;
+	private String password;
+	private String first_name;
+	private String last_name;
+	private String phone_num;
+	private String email;
+	private String address;  
+	//private UserAccount userAcct; //bi-directional relation
+	private List<BankAccount> baList; // lists for bank accounts
+ 	
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getPhone_num() {
+		return phone_num;
+	}
+	public void setPhone_num(String phone_num) {
+		this.phone_num = phone_num;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
