3ec3f07c391eb408227dd901f604bdcb42765248
ungsneelima <164307239+ungsneelima@users.noreply.github.com>
2025-02-26 11:44:46 +0530

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,69 @@
+package com.sat.tmf.pmt.entity;
+
+public class BankAccount {
+    private long bankAcctId;
+    private String acctNumber;
+    private String ifscCode;
+    //	private String acctStatus;
+    private BankAccountStatusEnum acctStatus; // Using Enum
+    private String bankName;
+    private String branch;
+
+    private User user; // Bi-directional relation
+
+    public long getBankAcctId() {
+        return bankAcctId;
+    }
+
+    public void setBankAcctId(long bankAcctId) {
+        this.bankAcctId = bankAcctId;
+    }
+
+    public String getAcctNumber() {
+        return acctNumber;
+    }
+
+    public void setAcctNumber(String acctNumber) {
+        this.acctNumber = acctNumber;
+    }
+
+    public String getIfscCode() {
+        return ifscCode;
+    }
+
+    public void setIfscCode(String ifscCode) {
+        this.ifscCode = ifscCode;
+    }
+
+    public BankAccountStatusEnum getAcctStatus() {
+        return acctStatus;
+    }
+
+    public void setAcctStatus(BankAccountStatusEnum acctStatus) {
+        this.acctStatus = acctStatus;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
